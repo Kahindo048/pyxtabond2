393df5cc57ab74d2127e99734d6baf911c5147f0
Frederick Kahindo <163439905+Kahindo048@users.noreply.github.com>
2026-08-25 13:17:23 +0100

    release of v0.0.3

--- pyxtabond2/__init__.py
+++ pyxtabond2/__init__.py
@@ -28,7 +28,7 @@ from .exporter import GMMStargazer
 from .load_data import list_datasets, load_dataset
 from .specs import GMMStyle, IVStyle
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = "Frederick Kahindo"
 
 __all__ = [
